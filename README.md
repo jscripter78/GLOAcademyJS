89979d9dbcb9612e7e73bd0d2b2242e4b2f02873
Andrey <kurkov-am@yandex.ru>
2019-12-02 00:05:12 +0300

    first needed homework

---
+++ README.md
@@ -0,0 +1,3 @@
+GLOAcademy
+Course JavaScript
+First Lesson
---
+++ script.js
@@ -0,0 +1,10 @@
+let money;
+let income; 
+let addExpenses;
+let deposit; 
+let mission; 
+let period;
+
+alert('This is "alert"!');
+
+console.log('This is some text into console');
